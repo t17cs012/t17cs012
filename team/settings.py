e085c1828286fdd3e46f39531908274dc10faeb3
t17cs012 <t17cs012@yamanashi.ac.jp>
2020-02-03 13:58:17 +0900

    settings

--- team/settings.py
+++ team/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hb(+4sj#04yp4)1hyatfk$_dz+6!m-^lgexm17c2xx1pszh$$$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 LOGIN_REDIRECT_URL = 'admin_list'
 
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BAS_DIR, 'static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
